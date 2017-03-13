d4113183956fb604677fee6fc9bb857185de5cea
Leo Young <420760135@qq.com>
2017-03-14 00:55:22 +0800

    add config to settings.json

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,11 @@
 /**
  * Config
  */
+
+import * as vscode from 'vscode';
 class Config {
-    public static readonly APP_ID = "20170225000039853";
-    public static readonly KEY = "usbSw2CPxPSIwKPDazaZ";
+    public static readonly APP_ID: string = vscode.workspace.getConfiguration('leoTranslator').get('appId') as string;
+    public static readonly KEY: string = vscode.workspace.getConfiguration('leoTranslator').get('key') as string;
 }
 
 export { Config };
